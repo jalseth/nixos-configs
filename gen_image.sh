8f07f2a0d7a683ad87685475482cb43b6cc5526e
James Alseth <james@jalseth.me>
2024-02-10 17:23:56 -0800

    Add disk-size option

--- gen_image.sh
+++ gen_image.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 config_path=""
 channel_version=""
 format=""
+nixos_build_args=()
 
 while [[ $# -gt 0 ]]; do
   case "${1}" in
@@ -20,14 +21,19 @@ while [[ $# -gt 0 ]]; do
       channel_version="${2}"
       shift
       ;;
+    -d | --disk_size)
+      nixos_build_args+=("--disk-size" "${2}")
+      shift
+      ;;
     *)
-      abort "unknown option $1"
+      abort "unknown option ${1}"
       ;;
   esac
   shift
 done
 
-out_path=$(nix run github:nix-community/nixos-generators -- \
+out_path=$(nix run github:jalseth/nixos-generators -- \
+  "${nixos_build_args[@]}" \
   -I "nixpkgs=channel:${channel_version}" \
   -f "${format}" \
   -c "${config_path}")
